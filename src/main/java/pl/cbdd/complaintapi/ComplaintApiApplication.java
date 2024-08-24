8b80f099fb2ed14e683f606f5ec260f809ce95c6
Dawid Drozdz <codebydawiddrozdz@gmail.com>
2024-08-24 15:52:54 +0200

    Initial commit

---
+++ src/main/java/pl/cbdd/complaintapi/ComplaintApiApplication.java
@@ -0,0 +1,13 @@
+package pl.cbdd.complaintapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/cbdd/complaintapi/ComplaintApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cbdd.complaintapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
